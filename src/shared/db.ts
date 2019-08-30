02acc52a4237566037c1f70d6edcacf9313efea0
kerry okpere <kerryokpere@gmail.com>
2019-08-30 15:28:12 +0100

    Update: remove comments

--- src/shared/db.ts
+++ src/shared/db.ts
@@ -1,7 +1,6 @@
 const Sequelize = require('sequelize');
 import { DB_HOST, DB_NAME, DB_PASS, DB_USER } from "../config/env";
 
-// Option 1: Passing parameters separately
 export const connection = new Sequelize( DB_NAME,  DB_USER, DB_PASS, {
   host: DB_HOST,
   dialect: 'mysql'
